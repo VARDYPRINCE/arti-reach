6d0ae0ef87310c457b98a54277411a8255a52791
kingboymaylow <henryokpe8@gmail.com>
2024-07-31 06:38:58 -0700

    auth feature initial commit

--- .history/src/App_20240731054427.jsx
+++ .history/src/App_20240731054427.jsx
@@ -1,8 +1,6 @@
 import "./App.css";
 import AboutUs from "./Pages/AboutUs";
 import LandingPage from "./Pages/LandingPage";
-import RegistrationForm from "./Pages/RegistrationForm";
-import LoginForm from "./Pages/LoginForm";
 import Service from "./Pages/Service";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -12,8 +10,6 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          <Route path="/register"  element={<RegistrationForm/>} />
-        <Route path="/login" element={<LoginForm/>} />
         </Routes>
       </BrowserRouter>
       <AboutUs />
